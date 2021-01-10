a33b813c000026ec91c9169e975657ba1ba9e300
Taylor Harrison Carroll <taylorhcarroll1@gmail.com>
2021-01-10 12:35:47 -0600

    update

--- tut1/pytorchWithExamples.py
+++ tut1/pytorchWithExamples.py
@@ -1,58 +1,84 @@
-# test
 import torch
 import math
+
+
+class LegendrePolynomial3(torch.autograd.Function):
+    """
+    we implement our own custom autograd functions by subclassing
+    torch.autograd.function and implementing the forward and backward passes
+    which operate on Tensors.
+    """
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        in the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the output. ctx is a context object that can be used
+        to stash information for backward computation. You can cache arbitrary
+        objects for use in the backward pass using the ctx.save_for_backward method.
+        """
+        ctx.save_for_backward(input)
+        return 0.5 * (5 * input ** 3 - 3 * input)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the loss
+        with the respect to the output, and we need to compute the gradient of the loss
+        with respect to the input.
+        """
+        input, = ctx.saved_tensors
+        return grad_output * 1.5 * (5 * input ** 2 - 1)
 
 
 dtype = torch.float
 device = torch.device("cpu")
 
-# we create tensors to hold our input and outputs
+# creates tensors to hold input/output
 # By default, requires_grad=False, which indicates that we do not need to
 # compute gradients with respect to these Tensors during the backward pass.
 x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
 y = torch.sin(x)
 
-# Create random Tensors for weights. For a third order polynomial, we need
-# 4 weights: y = a + b x + c x^2 + d x^3
+# create 4 random tensors for weights
+# For this example, we need 4 weights: y = a + b * P3(c + d * x),
+# these weights need to be initialized
+# not too far from the correct result to ensure convergence.
 # Setting requires_grad=True indicates that we want to compute gradients with
 # respect to these Tensors during the backward pass.
-a = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
+a = torch.full((), 0.0, device=device, dtype=dtype, requires_grad=True)
+b = torch.full((), -1.0, device=device, dtype=dtype, requires_grad=True)
+c = torch.full((), 0.0, device=device, dtype=dtype, requires_grad=True)
+d = torch.full((), 0.3, device=device, dtype=dtype, requires_grad=True)
 
-learning_rate = 1e-6
+learning_rate = 5e-6
 for t in range(2000):
-    # Forward pass: compute predicted y using operations on Tensors.
-    y_pred = a + b * x + c * x ** 2 + d * x ** 3
+    # To apply our function, we use function.apply method, we alias this as 'P3'
+    P3 = LegendrePolynomial3.apply
 
-    # Compute and print loss using operations on Tensors.
-    # Now loss is a Tensor of shape (1,)
-    # loss.item() gets the scalar value held in the loss.
+    # Forward pass: compute predicted y using operations; we compute
+    # P3 using our custom autograd operation
+    y_pred = a + b * P3(c + d * x)
+
+    # Compute and print loss
     loss = (y_pred - y).pow(2).sum()
     if t % 100 == 99:
         print(t, loss.item())
 
-    # Use autograd to compute the backward pass. This call will compute the
-    # gradient of loss with respect to all Tensors with requires_grad=True.
-    # After this call a.grad, b.grad. c.grad and d.grad will be Tensors holding
-    # the gradient of the loss with respect to a, b, c, d respectively.
+    # use autograd to compute the backward pass
     loss.backward()
 
-    # Manually update weights using gradient descent. Wrap in torch.no_grad()
-    # because weights have requires_grad=True, but we don't need to track this
-    # in autograd.
+    # update weights using gradient descent
     with torch.no_grad():
         a -= learning_rate * a.grad
         b -= learning_rate * b.grad
         c -= learning_rate * c.grad
         d -= learning_rate * d.grad
 
-        # Manually zero the gradients after updating weights
+        # manually zero gradients after updating weights
         a.grad = None
         b.grad = None
         c.grad = None
         d.grad = None
 
-print(
-    f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
+print(f'Result: y = {a.item()} + {b.item()} * P3({c.item} + {d.item()} x)')
